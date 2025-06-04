9ff23f02ab8cff07d860554bdae70fc10881b8a6
Cristian <cristiaansantacruz@gmail.com>
2025-06-04 10:21:42 -0400

    Consumiendo los productos en la pagina principal

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from apps.Productos.models import Categoria, Producto
 
 def index(request):
-    return render(request, "index.html")
-# Create your views here.
+    categorias = Categoria.objects.all()
+    
+    productos_por_categoria = {
+        categoria: list(filter(lambda p: p.categoria == categoria, Producto.objects.all()))
+        for categoria in categorias
+    }
+
+    context = {
+        'categorias': categorias,
+        'productos_por_categoria': productos_por_categoria,
+    }
+    return render(request, "index.html", context)
